38bad38f5f13b337803d640f2050023e64fbb9c3
Matt Heitker <heitkergm@acm.org>
2026-08-31 22:30:09 -0400

    add custom eerror controller

---
+++ src/main/java/com/dappermoose/finance/init/MyErrorController.java
@@ -0,0 +1,35 @@
+package com.dappermoose.finance.init;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MyErrorController.
+ */
+@Controller
+public class MyErrorController implements ErrorController
+{
+    /**
+     * Error action.
+     *
+     * @param request - the servlet request
+     * @param model the model
+     * @return the string
+     */
+    @RequestMapping ("/error")
+    public String myErrorAction (final HttpServletRequest request,
+            final Model model)
+    {
+        model.addAttribute ("status",
+                request.getAttribute ("jakarta.servlet.error.status_code"));
+        model.addAttribute ("reason",
+                request.getAttribute ("jakarta.servlet.error.message"));
+
+        return "httpError";
+    }
+}
